334e1ad23a797922fa646cc9b0b28054f050ce50
Ubuntu <ubuntu@ip-172-31-1-41.us-west-1.compute.internal>
2017-01-12 07:46:07 +0000

    backendserver update 0112

--- backendserver/lua/ramlab_upload2file.lua
+++ backendserver/lua/ramlab_upload2file.lua
@@ -1,7 +1,7 @@
 local resty_md5 = require "resty.md5"
 local upload = require "resty.upload"
 local str = require "resty.string"
-local dst_dir = "/home/ubuntu/load_balancing_http/img"
+local dst_dir = "/home/ubuntu/webserver/backendserver/img"
 
 
 function get_filename(res)
